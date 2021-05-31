27c15345afb37235ff37ea3d12181b1905d076e7
MinJungi <als15687@naver.com>
2021-05-31 12:18:27 +0900

    ver1.0.0
    no limit version

---
+++ cmake-build-debug/main/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/src/backtrack.cc" "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/cmake-build-debug/main/CMakeFiles/program.dir/__/src/backtrack.cc.obj"
+  "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/src/candidate_set.cc" "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/cmake-build-debug/main/CMakeFiles/program.dir/__/src/candidate_set.cc.obj"
+  "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/src/graph.cc" "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/cmake-build-debug/main/CMakeFiles/program.dir/__/src/graph.cc.obj"
+  "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/main/main.cc" "C:/Users/als15/Documents/GitHub/Graph-Pattern-Matching-Challenge/cmake-build-debug/main/CMakeFiles/program.dir/main.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- include/backtrack.h
+++ include/backtrack.h
@@ -2,7 +2,7 @@
  * @file backtrack.h
  *
  */
-
+/*
 #ifndef BACKTRACK_H_
 #define BACKTRACK_H_
 
@@ -17,6 +17,45 @@ class Backtrack {
 
   void PrintAllMatches(const Graph &data, const Graph &query,
                        const CandidateSet &cs);
+};
+
+#endif  // BACKTRACK_H_
+*/
+
+/**
+ * @file backtrack.h
+ *
+ */
+
+#ifndef BACKTRACK_H_
+#define BACKTRACK_H_
+
+#include <stack>
+#include "candidate_set.h"
+#include "common.h"
+#include "graph.h"
+
+class Backtrack {
+public:
+    Backtrack();
+    ~Backtrack();
+
+    void PrintAllMatches(const Graph &data, const Graph &query, const CandidateSet &cs);
+
+    void recur(Vertex curr_embedding[], Vertex prev_matched, std::vector<Vertex> old_ext_vertex, std::vector<std::vector<std::pair<Vertex, Vertex>>> old_ext_candidate,
+               const Graph &data, const Graph &query, const CandidateSet &cs,
+               const std::vector<std::vector<Vertex>> &query_dag, const std::vector<std::vector<Vertex>> &query_dag_inv);
+
+    bool is_extendable_candidate(std::pair<Vertex, Vertex> candidate, const Vertex curr_embedding[], const Graph &data, const std::vector<std::vector<Vertex>> &query_dag_inv);
+
+    std::pair<std::vector<Vertex>, std::vector<std::vector<std::pair<Vertex, Vertex>>>> get_new_extendable_pair(std::vector<Vertex> old_extendable_vertex,
+                                                                                                                std::vector<std::vector<std::pair<Vertex, Vertex>>> old_extendable_candidate,
+                                                                                                                Vertex matched, const Vertex curr_embedding[], const Graph &data, const CandidateSet &cs,
+                                                                                                                const std::vector<std::vector<Vertex>> &query_dag,
+                                                                                                                const std::vector<std::vector<Vertex>> &query_dag_inv);
+
+    void print_embedding(const Vertex curr_embedding[], int size);
+
 };
 
 #endif  // BACKTRACK_H_
